3689c66c2981c12405f54fd9e3768402c1687dbf
Peter Tang <petersktang@gmail.com>
2016-07-02 15:45:11 +0800

    Turn ConferderationController into Async Rest API

--- src/main/java/com/jordanec/sbrestapistormpath/api/ConfederationController.java
+++ src/main/java/com/jordanec/sbrestapistormpath/api/ConfederationController.java
@@ -3,6 +3,8 @@ package com.jordanec.sbrestapistormpath.api;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jordanec.sbrestapistormpath.model.*;
 import com.jordanec.sbrestapistormpath.service.ConfederationService;
 import com.jordanec.sbrestapistormpath.util.Constants;
+
+import java.util.concurrent.Future;
 
 @RestController
 @RequestMapping("/"+Constants.API_VERSION+"/")
@@ -22,37 +26,44 @@ public class ConfederationController {
 	static final Logger logger = Logger.getLogger(ConfederationController.class);
 	
 	@RequestMapping(value =Constants.CONFEDERATIONS_PATH, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	CustomRequRespObject<Confederation> createConfederation(@RequestBody CustomRequRespObject<Confederation> customRequRespConfederation) {
-		return confederationService.createConfederation(customRequRespConfederation);
+	@Async
+	Future<CustomRequRespObject<Confederation>> createConfederation(@RequestBody CustomRequRespObject<Confederation> customRequRespConfederation) {
+		return new AsyncResult<CustomRequRespObject<Confederation>> (confederationService.createConfederation(customRequRespConfederation));
 	}
 	
 	@RequestMapping(value=Constants.CONFEDERATIONS_PATH+"/{id}", method= RequestMethod.GET)
-	CustomRequRespObject<Confederation> readConfederation(@PathVariable("id") int id) {
-		return confederationService.readConfederation(id);
+	@Async
+	Future<CustomRequRespObject<Confederation>> readConfederation(@PathVariable("id") int id) {
+		return new AsyncResult<CustomRequRespObject<Confederation>> (confederationService.readConfederation(id));
 	}
 	
 	@RequestMapping(value=Constants.CONFEDERATIONS_PATH+"/name={name}", method=RequestMethod.GET)
-	CustomRequRespObject<Confederation> findByName(@PathVariable("name") String name) {
-		return confederationService.findByName(name);
+	@Async
+	Future<CustomRequRespObject<Confederation>> findByName(@PathVariable("name") String name) {
+		return new AsyncResult<CustomRequRespObject<Confederation>> (confederationService.findByName(name));
 	}
 	
 	@RequestMapping(value=Constants.CONFEDERATIONS_PATH+"/{id}", method=RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-	CustomRequRespObject<Confederation> updateConfederation(@RequestBody CustomRequRespObject<Confederation> customRequRespConfederation, @PathVariable int id) {
-		return confederationService.updateConfederation(customRequRespConfederation, id);
+	@Async
+	Future<CustomRequRespObject<Confederation>> updateConfederation(@RequestBody CustomRequRespObject<Confederation> customRequRespConfederation, @PathVariable int id) {
+		return new AsyncResult<CustomRequRespObject<Confederation>> (confederationService.updateConfederation(customRequRespConfederation, id));
 	}
 	
 	@RequestMapping(value=Constants.CONFEDERATIONS_PATH+"/{id}", method=RequestMethod.DELETE)
-	CustomRequRespObject<Confederation> deleteConfederation(@PathVariable int id) {
-		return confederationService.deleteConfederation(id);
+	@Async
+	Future<CustomRequRespObject<Confederation>> deleteConfederation(@PathVariable int id) {
+		return new AsyncResult<CustomRequRespObject<Confederation>> (confederationService.deleteConfederation(id));
 	}
-	
+
 	@RequestMapping(value=Constants.CONFEDERATIONS_PATH, method=RequestMethod.GET)
-	CustomRequRespObject<Confederation> listConfederations() {
-		return confederationService.listConfederations();
+	@Async
+	Future<CustomRequRespObject<Confederation>> listConfederations() {
+		return new AsyncResult<CustomRequRespObject<Confederation>> (confederationService.listConfederations());
 	}
 
 	@RequestMapping(value=Constants.CONFEDERATIONS_PATH+"/{id}/"+Constants.COUNTRIES_PATH, method= RequestMethod.GET)
-	CustomRequRespObject<Country> readConfederationCountries(@PathVariable("id") int id) {
-		return confederationService.readConfederationCountries(id);
+	@Async
+	Future<CustomRequRespObject<Country>> readConfederationCountries(@PathVariable("id") int id) {
+		return new AsyncResult<CustomRequRespObject<Country>> (confederationService.readConfederationCountries(id));
 	}
 }
---
+++ src/main/java/com/jordanec/sbrestapistormpath/clientAsync/AuthApi.java
@@ -0,0 +1,10 @@
+package com.jordanec.sbrestapistormpath.clientAsync;
+
+import com.jordanec.sbrestapistormpath.model.Token;
+
+/**
+ * Created by petersktang on 2/7/16.
+ */
+public class AuthApi {
+    Callback<Token> token ()
+}
